49e720b9a7eab6b454a8766ee50816f782cfa5a7
Jose-Miguel-Silva <65625556+Jose-Miguel-Silva@users.noreply.github.com>
2020-06-07 15:17:16 +0100

    Alteração das funcionalidades Árvore

--- Trabalhos/Trabalho.java
+++ Trabalhos/Trabalho.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Trabalho {
 
 	static boolean fim = false;
+	static String [][] resumoChamada = {
+  									   {"Ligou devido a um assunto não relacionado ao Covid-19.","?"},{"Ligou devido ao vírus Covid-19.","?"},
+    								   {"Não apresenta sintomas.","?"},{"Foi informado dos cuidados a ter com o vírus.","?"},
+  									   {"Apresenta sintomas do vírus ou, conhece alguém que apresenta os sintomas.","?"},{"Foi informado dos cuidados a ter com o vírus.","?"},
+  									   {"Declarou febre como sintoma apresentado.","?"},{"Foi informado dos cuidados a ter com o vírus.","?"},
+    								   {"Declarou tosse regular como sintoma apresentado. Chamada reencaminhada para um operador.","?"}
+  									   };
+									   //ordem -> 0,4,3,2,6,5,8,7,9
 	
 	public static void main(String[] args) {
 
@@ -45,20 +53,13 @@ public class Trabalho {
 		
 		}while (fim == false);
 		
+		imprimirResumo();
 		
 		
 		ler.close();
 	}
 	
-	private static boolean atendedorHandler(NoBinTree no, int valor) {
-		
-		if(no.getFilhoEsquerda() != null && no.getFilhoDireita() != null)
-			if(no.getFilhoEsquerda().getValor() == valor || no.getFilhoDireita().getValor() == valor)
-				return true;
-		
-		
-		return false;
-	}
+	
 	
 	private static String mensagemAuto(int valor) {
 		
@@ -69,53 +70,63 @@ public class Trabalho {
 			case 0:
 				mensagem = "Devido ao elevado número de chamadas não podemos responder ao seu pedido de momento, por favor ligue mais tarde.";
 				fim = true;
+				resumoChamada[0][1] ="sim";
 				break;
 				
 			case 1:
-				mensagem = "Ligou para a linha de saúde 24, devido aos eventos recentes e ao tráfego resultante estamos a dar prioridade a casos de Covid-19."
-						+ "Se ligou devido ao Covid-19 prima 4, caso contrário, prima 0. Para cancelar, prima outra tecla.";
+				mensagem = "Ligou para a linha de saúde 24, devido aos eventos recentes e ao tráfego resultante estamos a dar prioridade a casos de Covid-19.\n"
+						 + "Se ligou devido ao Covid-19 prima 4, caso contrário, prima 0. Para cancelar, prima outra tecla.";
 				break;
 				
 			case 2:
 				mensagem = "Mensagem definida para como ficar seguro do Covid-19";
 				fim = true;
+				resumoChamada[3][1] ="sim";
 				break;
 				
 			case 3:
-				mensagem = "O virús Covid-19 apresenta como principais sintomas: tosse, febre(temperatura >= 38oC) e dificuldade respiratória (ex. falta de ar)."
+				mensagem = "O virús Covid-19 apresenta como principais sintomas: tosse, febre(temperatura >= 38oC) e dificuldade respiratória (ex. falta de ar).\n"
 						+ "Para continuar prima 2. Para cancelar, prima outra tecla.";
+				resumoChamada[2][1] ="sim";
 				break;
 				
 			case 4:
-				mensagem = "Apresenta sintomas do virus covid-19, ou conhece alguém que apresenta sintomas? Se sim, prima 6, se não, prima 3.";
+				mensagem = "Apresenta sintomas do virus covid-19, ou conhece alguém que apresenta sintomas?\n Se sim, prima 6, se não, prima 3.";
+				resumoChamada[1][1] ="sim";
 				break;
 				
 			case 5:
 				mensagem = "Mensagem definida para como ficar seguro do Covid-19";
 				fim = true;
+				resumoChamada[5][1] ="sim";
 				break;
 				
 			case 6:
-				mensagem = "A pessoa em causa apresenta sintomas de febre(temperatura >= 38oC)? Se sim, prima 8, se não, prima 5.";
+				mensagem = "A pessoa em causa apresenta sintomas de febre(temperatura >= 38oC)?\n Se sim, prima 8, se não, prima 5.";
+				resumoChamada[4][1] ="sim";
 				break;
 				
 			case 7:
 				mensagem = "Mensagem definida para como ficar seguro do Covid-19";
 				fim = true;
+				resumoChamada[7][1] ="sim";
 				break;
 				
 			case 8:
-				mensagem = "A pessoa em causa tosse com regularidade? Se sim, prima 9, se não, prima 7.";
+				mensagem = "A pessoa em causa tosse com regularidade?\n Se sim, prima 9, se não, prima 7.";
+				resumoChamada[6][1] ="sim";
 				break;
 				
 			case 9:
 				mensagem = "A sua chamada vai ser reencaminhada, por favor aguarde.";
 				fim = true;
+				resumoChamada[8][1] ="sim";
 				break;	
 		}
 		
 		
 		return mensagem;
 	}
-
+	
+	
 }
